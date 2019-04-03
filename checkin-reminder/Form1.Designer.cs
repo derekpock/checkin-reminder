d971c42c6e1a8dd962a464ec4aeef326dcdd2801
Derek Pockrandt <derek.pockrandt@garmin.com>
2019-04-03 13:06:59 -0500

    Added timer to keep track of hours

--- checkin-reminder/Form1.Designer.cs
+++ checkin-reminder/Form1.Designer.cs
@@ -30,11 +30,17 @@
         {
             this.butCheck = new System.Windows.Forms.Button();
             this.labStatus = new System.Windows.Forms.Label();
+            this.labElapsed = new System.Windows.Forms.Label();
+            this.labElapsedDesc = new System.Windows.Forms.Label();
+            this.labRemainDesc = new System.Windows.Forms.Label();
+            this.labRemain = new System.Windows.Forms.Label();
+            this.labFinishDesc = new System.Windows.Forms.Label();
+            this.labFinish = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // butCheck
             // 
-            this.butCheck.Location = new System.Drawing.Point(12, 25);
+            this.butCheck.Location = new System.Drawing.Point(15, 80);
             this.butCheck.Name = "butCheck";
             this.butCheck.Size = new System.Drawing.Size(183, 41);
             this.butCheck.TabIndex = 0;
@@ -51,14 +57,78 @@
             this.labStatus.TabIndex = 1;
             this.labStatus.Text = "label1";
             // 
+            // labElapsed
+            // 
+            this.labElapsed.AutoSize = true;
+            this.labElapsed.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labElapsed.Location = new System.Drawing.Point(103, 26);
+            this.labElapsed.Name = "labElapsed";
+            this.labElapsed.Size = new System.Drawing.Size(56, 15);
+            this.labElapsed.TabIndex = 2;
+            this.labElapsed.Text = "0:00:00";
+            // 
+            // labElapsedDesc
+            // 
+            this.labElapsedDesc.AutoSize = true;
+            this.labElapsedDesc.Location = new System.Drawing.Point(12, 27);
+            this.labElapsedDesc.Name = "labElapsedDesc";
+            this.labElapsedDesc.Size = new System.Drawing.Size(74, 13);
+            this.labElapsedDesc.TabIndex = 3;
+            this.labElapsedDesc.Text = "Elapsed Time:";
+            // 
+            // labRemainDesc
+            // 
+            this.labRemainDesc.AutoSize = true;
+            this.labRemainDesc.Location = new System.Drawing.Point(11, 45);
+            this.labRemainDesc.Name = "labRemainDesc";
+            this.labRemainDesc.Size = new System.Drawing.Size(86, 13);
+            this.labRemainDesc.TabIndex = 4;
+            this.labRemainDesc.Text = "Time Remaining:";
+            // 
+            // labRemain
+            // 
+            this.labRemain.AutoSize = true;
+            this.labRemain.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labRemain.Location = new System.Drawing.Point(103, 44);
+            this.labRemain.Name = "labRemain";
+            this.labRemain.Size = new System.Drawing.Size(56, 15);
+            this.labRemain.TabIndex = 5;
+            this.labRemain.Text = "0:00:00";
+            // 
+            // labFinishDesc
+            // 
+            this.labFinishDesc.AutoSize = true;
+            this.labFinishDesc.Location = new System.Drawing.Point(11, 63);
+            this.labFinishDesc.Name = "labFinishDesc";
+            this.labFinishDesc.Size = new System.Drawing.Size(75, 13);
+            this.labFinishDesc.TabIndex = 6;
+            this.labFinishDesc.Text = "Time Finished:";
+            // 
+            // labFinish
+            // 
+            this.labFinish.AutoSize = true;
+            this.labFinish.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labFinish.Location = new System.Drawing.Point(103, 62);
+            this.labFinish.Name = "labFinish";
+            this.labFinish.Size = new System.Drawing.Size(42, 15);
+            this.labFinish.TabIndex = 7;
+            this.labFinish.Text = "16:40";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(207, 78);
+            this.ClientSize = new System.Drawing.Size(207, 133);
             this.ControlBox = false;
+            this.Controls.Add(this.labFinish);
+            this.Controls.Add(this.labFinishDesc);
+            this.Controls.Add(this.labRemain);
+            this.Controls.Add(this.labRemainDesc);
+            this.Controls.Add(this.labElapsedDesc);
+            this.Controls.Add(this.labElapsed);
             this.Controls.Add(this.labStatus);
             this.Controls.Add(this.butCheck);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Checkin Reminder";
@@ -72,6 +142,12 @@
 
         private System.Windows.Forms.Button butCheck;
         private System.Windows.Forms.Label labStatus;
+        private System.Windows.Forms.Label labElapsed;
+        private System.Windows.Forms.Label labElapsedDesc;
+        private System.Windows.Forms.Label labRemainDesc;
+        private System.Windows.Forms.Label labRemain;
+        private System.Windows.Forms.Label labFinishDesc;
+        private System.Windows.Forms.Label labFinish;
     }
 }
 
--- checkin-reminder/Form1.cs
+++ checkin-reminder/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Threading;
 
 namespace checkin_reminder
 {
@@ -16,15 +17,78 @@ namespace checkin_reminder
         CheckedIn,
         CheckedOut
     }
+
     public partial class Form1 : Form
     {
         CheckinState state;
+        DateTime today;
+        TimeSpan workedTime;
+        Dictionary<DayOfWeek, TimeSpan> schedule;
+        
+
+        delegate void SetTextCallback();
+
+        private void SetLabelsText()
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (labElapsed.InvokeRequired)
+            {
+                this.Invoke(new SetTextCallback(SetLabelsText));
+            }
+            else
+            {
+                labElapsed.Text = workedTime.ToString("h\\:mm\\:ss");
+                labRemain.Text = schedule[today.DayOfWeek].Subtract(workedTime).ToString("h\\:mm\\:ss");
+                labFinish.Text = today.Add(schedule[today.DayOfWeek].Subtract(workedTime)).ToShortTimeString();
+            }
+        }
 
         public Form1()
         {
             InitializeComponent();
+            schedule = new Dictionary<DayOfWeek, TimeSpan>();
+            schedule.Add(DayOfWeek.Sunday,    new TimeSpan(0, 00, 0));
+            schedule.Add(DayOfWeek.Monday,    new TimeSpan(5, 30, 0));
+            schedule.Add(DayOfWeek.Tuesday,   new TimeSpan(0, 00, 0));
+            schedule.Add(DayOfWeek.Wednesday, new TimeSpan(4, 00, 0));
+            schedule.Add(DayOfWeek.Thursday,  new TimeSpan(0, 00, 0));
+            schedule.Add(DayOfWeek.Friday,    new TimeSpan(5, 30, 0));
+            schedule.Add(DayOfWeek.Saturday,  new TimeSpan(0, 00, 0));
+
             state = CheckinState.CheckedOut;
+            today = DateTime.Now;
+            workedTime = TimeSpan.Zero;
             prepareState(state);
+
+            Thread timeThread = new Thread(new ThreadStart(this.TimeThread));
+            timeThread.IsBackground = true;
+            timeThread.Start();
+        }
+
+        private void TimeThread()
+        {
+            while(true)
+            {
+                if (DateTime.Now.DayOfWeek != today.DayOfWeek)
+                {
+                    workedTime = TimeSpan.Zero;
+                }
+
+                switch (state)
+                {
+                    case CheckinState.CheckedIn:
+                        workedTime = DateTime.Now.Subtract(today);
+                        break;
+
+                    case CheckinState.CheckedOut:
+                        today = DateTime.Now.Subtract(workedTime);
+                        break;
+                }
+                SetLabelsText();
+                Thread.Sleep(1000);
+            }
         }
 
         private void butCheck_Click(object sender, EventArgs e)
